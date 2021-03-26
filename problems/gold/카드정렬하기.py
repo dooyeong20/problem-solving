66e61be5e3c63b4fcb69a406f2d9137fe28c0f49
dooyeong20 <noviced22@gmail.com>
2021-03-26 10:58:30 +0900

    feat: Solved problem gold 4
    Data Structure - priority queue

---
+++ problems/gold/카드정렬하기.py
@@ -0,0 +1,26 @@
+from sys import stdin
+import heapq
+
+
+def solution(n, cards):
+    if n == 1:
+        return 0
+
+    heapq.heapify(cards)
+
+    ans = 0
+    tmp = []
+
+    while n > 1:
+        tmp = heapq.heappop(cards) + heapq.heappop(cards)
+        ans += tmp
+        heapq.heappush(cards, tmp)
+        n -= 1
+
+    return ans
+
+
+n = int(input())
+cards = [int(stdin.readline()) for _ in range(n)]
+
+print(solution(n, cards))
